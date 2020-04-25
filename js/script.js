447e7a17b07a0f16f02857f0898ba0a2976459f2
Bagas17k <kurniajati@alterra.id>
2020-04-25 15:11:52 +0700

    add questionanswer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+let listAnswer = [
+  'Sholeh',
+  'Aji'
+]
+
+let question = [
+  'Anak ekspresif?',
+  'Anak insecure?'
+]
+
+let playerAnswer = '';
+let guessed = [];
+let maxWrong = 6;
+let mistakes = 0;
+let wordStatus = null;
+
+randomIndex = Math.floor(Math.random() * listAnswer.length);
+
+function generateQuestion(){
+  questions = question[randomIndex]
+  document.getElementById('question').innerHTML = questions
+}
+
+function randomAnswer(){
+  playerAnswer = listAnswer[randomIndex]
+
+}
+
 function generateButtons() {
     let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
         `
@@ -13,4 +41,29 @@ function generateButtons() {
     document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
+
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+  }
+}
+
+function guessedWord(){
+  wordStatus = playerAnswer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+  document.getElementById('wordSpotlight').innerHTML = wordStatus
+}
+
+generateQuestion();
+randomAnswer();
 generateButtons();
+guessedWord();
+
